dfc4a42af7a9ff47ce1ec74595435c3d8275ca37
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-08-02 15:16:43 +0100

    adding weather

--- jasmine/spec/FeatureSpec.js
+++ jasmine/spec/FeatureSpec.js
@@ -9,19 +9,36 @@ describe('Feature Test:', function() {
     plane = new Plane();
   });
 
-  it('planes can be instructed to land at an airport', function() {
-    plane.land(airport);
-    expect(airport.planes()).toContain(plane);
-  })
-  it('planes can be instructed to take off from an airport', function() {
-    plane.land(airport);
-    plane.takeoff();
-    expect(airport.planes()).not.toContain(plane);
-  })
-  it('stop takeoff when weather is stormy', function() {
-    plane.land(airport);
-    spyOn(airport, 'isStormy').and.returnValue(true);
-    expect(function(){ plane.takeoff();}).toThrowError('cannot take off during storm');
-    expect(airport.planes()).toContain(plane);
-  })
+  describe('under normal conditions', function(){
+    beforeEach(function(){
+      spyOn(Math,'random').and.returnValue(0);
+    });
+
+    it('planes can be instructed to land at an airport', function(){
+      plane.land(airport);
+      expect(airport.planes()).toContain(plane);
+    });
+
+    it('planes can be instructed to take off from an airport', function(){
+      plane.land(airport);
+      plane.takeoff();
+      expect(airport.planes()).not.toContain(plane);
+    });
+  });
+
+  describe('under stormy conditions', function(){
+    it('stop takeoff when weather is stormy', function(){
+      spyOn(Math, 'random').and.returnValue(0);
+      plane.land(airport);
+      spyOn(airport._weather, 'isStormy').and.returnValue(true);
+      expect(function(){ plane.takeoff();}).toThrowError('cannot take off during storm');
+      expect(airport.planes()).toContain(plane);
+    });
+
+    it('stops landing when weather is stormy', function(){
+      spyOn(Math,'random').and.returnValue(1);
+      expect(function(){ plane.land(airport);}).toThrowError('cannot land during storm');
+      expect(airport.planes()).toEqual([]);
+    });
+  });
 })
